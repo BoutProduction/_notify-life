552706e655c24bbc2d3c554802d8c54baaa78a41
HoodRusher <boutlpyt@gmail.com>
2024-03-14 16:37:04 +0100

    Update script.js

--- _notify-lite/html/script.js
+++ _notify-lite/html/script.js
@@ -8,7 +8,7 @@ $(function () {
 
     switch (action) {
       case "sendNotify":
-        Notify.trigger(max, title, msg, time, type);
+        Notify.trigger(title, msg, time, type);
         break;
       case "sendAnnounce":
         Announce.trigger(title, msg, time, type);
@@ -96,8 +96,8 @@ const Announce = {
 const Notify = {
   notifications: [],
 
-  trigger(max, title, message, duration, type) {
-    this.removeExcessNotifications(max);
+  trigger(title, message, duration, type) {
+    this.removeExcessNotifications();
 
     const id = `notification-${Math.random().toString(36).slice(2)}`;
     const el = this.createNotificationElement(id, title, message, type);
@@ -113,8 +113,8 @@ const Notify = {
     this.notifications.push(el);
   },
 
-  removeExcessNotifications(max) {
-    if (this.notifications.length >= max) {
+  removeExcessNotifications() {
+    if (this.notifications.length >= 6) {
       const removedNotification = this.notifications.shift();
       this.removeNotification(removedNotification);
     }
